8fd9b3f6122f1e7208bfd83c15ca8e3ecc1c3d5a
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-11 22:26:54 -0700

    add file main.py in 2_Constructor __init__()

---
+++ 2_Constructor __init__()/main.py
@@ -0,0 +1,15 @@
+# template
+class Estudent:
+	pass
+
+# object 
+auxilia = Estudent()
+auxilia.name = "auxilia"
+auxilia.id = 113
+
+aurito = Estudent()
+aurito.name = "aurito"
+aurito.id = 114
+
+print(auxilia.__dict__)
+print(aurito.__dict__)
