920d40b71a1df160407c10585b12c65ed1094109
jm-kodigu <juliaodejesusmartins@gmail.com>
2020-05-14 15:47:03 -0700

    learn encapsulation standar programming in 7_encapsulation standar, file main.py

--- 7_Encapsulation Standar/main.py
+++ 7_Encapsulation Standar/main.py
@@ -11,9 +11,37 @@ class Estudent:
 		self.__age = age
 		Estudent.__total += 1
 
+	# getter
+	def showname(self):
+		print(self.__name)
 
+	def showid(self):
+		return self.__id
+
+	def showinfo(self):
+		print("{} \n\t id : {} \n\t age : {} \n".format(self.__name,self.__id,self.__age))
+
+	# setter
+	def changename(self,newname):
+		self.__name = newname 
+
+	def changeid(self,newid):
+		self.__id = newid
 
 # object 
 ifania = Estudent("ifania",124,20)
 ilda = Estudent("ilda",125,20)
 januario = Estudent("januario",126,22)
+
+# ifania.showname()
+# print(ifania.showid())
+
+ifania.showinfo()
+
+ifania.changename('fania')
+ifania.changeid('100')
+
+ifania.showinfo()
+
+ilda.showname()
+print(januario.showid())
